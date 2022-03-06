3849b9e1ad5555e235f7c37be62bfaabb2f1b089
Fazekas Tamás <tamas.fzks@gmail.com>
2022-03-06 23:24:15 +0100

    3. verzió

--- feladat3.js
+++ feladat3.js
@@ -2,11 +2,13 @@ class Etel{
     #nev;
     #ar;
     #kategoria;
+    #osszetevok;
 
-    constructor(nev, ar, kategoria){
+    constructor(nev, ar, kategoria, osszetevok){
         this.#nev = nev;
         this.#ar = ar;
         this.#kategoria = kategoria;
+        this.#osszetevok = osszetevok;
     }
 
     get Nev() {
@@ -20,6 +22,10 @@ class Etel{
     get Kategoria() {
         return this.#kategoria;
     }
+    
+    get Osszetevok() {
+        return this.#osszetevok;
+    }
 }
 
 var etelek = [];
@@ -28,7 +34,8 @@ function felvetel(){
     let nev = document.getElementById('nev').value;
     let ar = document.getElementById('ar').value;
     let kategoria = document.getElementById('kategoria').value;
-    var etel = new Etel(nev, ar, kategoria);
+    let osszetevok = document.getElementById('osszetevok').value;
+    var etel = new Etel(nev, ar, kategoria, osszetevok);
     etelek.push(etel);
     console.table(etelek);
     tabla();
@@ -37,7 +44,7 @@ function felvetel(){
 function feltolt(){
     var szoveg = "";
     for (let i = 0; i < etelek.length; i++){
-        szoveg = szoveg + "<tr><td></td><td></td><td></td></tr>"
+        szoveg = szoveg + "<tr><td></td><td></td><td></td><td></td></tr>"
     }
     return szoveg;
 }
@@ -48,12 +55,13 @@ function tabla(){
     }
     szoveg = feltolt();
     var tab = document.createElement('table');
-    tab.innerHTML = '<tr><th class="text-center">Név</th><th class="text-center">Ár</th><th class="text-center">Kategória</th></tr>' + szoveg;
+    tab.innerHTML = '<tr><th class="text-center">Név</th><th class="text-center">Ár</th><th class="text-center">Kategória</th><th class="text-center">Összetevők</th></tr>' + szoveg;
     document.body.appendChild(tab);
     for (var i = 0; i < etelek.length; i++){
-        document.getElementsByTagName('td')[i * 3].innerText = etelek[i].Nev;
-        document.getElementsByTagName('td')[i * 3 + 1].innerText = etelek[i].Ar;
-        document.getElementsByTagName('td')[i * 3 + 2].innerText = etelek[i].Kategoria;
+        document.getElementsByTagName('td')[i * 4].innerText = etelek[i].Nev;
+        document.getElementsByTagName('td')[i * 4 + 1].innerText = etelek[i].Ar;
+        document.getElementsByTagName('td')[i * 4 + 2].innerText = etelek[i].Kategoria;
+        document.getElementsByTagName('td')[i * 4 + 3].innerText = etelek[i].Osszetevok;
     }
     
 }
